fccb5165f1e44eb0001c60ac904f8cdff0475c41
Dmitry Rashupkin <annexhack@gmail.com>
2022-06-07 14:28:45 +0300

    Fix getEvent

--- src/Traits/HasEvents.php
+++ src/Traits/HasEvents.php
@@ -21,57 +21,61 @@ use Webklex\PHPIMAP\Exceptions\EventNotFoundException;
  *
  * @package Webklex\PHPIMAP\Traits
  */
-trait HasEvents {
+trait HasEvents
+{
 
-    /**
-     * Event holder
-     *
-     * @var array $events
-     */
-    protected $events = [];
+	/**
+	 * Event holder
+	 *
+	 * @var array $events
+	 */
+	protected $events = [];
 
-    /**
-     * Set a specific event
-     * @param $section
-     * @param $event
-     * @param $class
-     */
-    public function setEvent($section, $event, $class) {
-        if (isset($this->events[$section])) {
-            $this->events[$section][$event] = $class;
-        }
-    }
+	/**
+	 * Set a specific event
+	 * @param $section
+	 * @param $event
+	 * @param $class
+	 */
+	public function setEvent($section, $event, $class)
+	{
+		if (isset($this->events[$section])) {
+			$this->events[$section][$event] = $class;
+		}
+	}
 
-    /**
-     * Set all events
-     * @param $events
-     */
-    public function setEvents($events) {
-        $this->events = $events;
-    }
+	/**
+	 * Set all events
+	 * @param $events
+	 */
+	public function setEvents($events)
+	{
+		$this->events = $events;
+	}
 
-    /**
-     * Get a specific event callback
-     * @param $section
-     * @param $event
-     *
-     * @return Event
-     * @throws EventNotFoundException
-     */
-    public function getEvent($section, $event): Event {
-        if (isset($this->events[$section])) {
-            return $this->events[$section][$event];
-        }
-        throw new EventNotFoundException();
-    }
+	/**
+	 * Get a specific event callback
+	 * @param $section
+	 * @param $event
+	 *
+	 * @return Event
+	 * @throws EventNotFoundException
+	 */
+	public function getEvent($section, $event)
+	{
+		if (isset($this->events[$section])) {
+			return $this->events[$section][$event];
+		}
+		throw new EventNotFoundException();
+	}
 
-    /**
-     * Get all events
-     *
-     * @return array
-     */
-    public function getEvents(): array {
-        return $this->events;
-    }
-
+	/**
+	 * Get all events
+	 *
+	 * @return array
+	 */
+	public function getEvents(): array
+	{
+		return $this->events;
+	}
 }
